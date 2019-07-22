03d6dda14951cb632bd21266b4a4ac0ca5402f9a
trevjnels <trevorjohnnels@gmail.com>
2019-07-22 16:00:44 -0700

    finally built a working ad too binary tree. Not self ballencing and feeding it pretty easy data

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 const expect = require('chai').expect;
+const fs = require('fs');
+const path = require('path');
 
 console.log(`------------BEGIN TESTS---------${Date()}---------------`);
 
@@ -9,14 +11,25 @@ describe('binary tree - functional', function() {
 	var tree = bstFunc();
 
 	it('should have an add node method', () => {
-		tree.addNode(1);
-		tree.addNode(2);
-		tree.addNode(42);
-		tree.addNode(55);
-		tree.addNode(199);
-		tree.addNode(200);
+		tree.addNode(10);
+		tree.addNode(9);
+		tree.addNode(11);
+		tree.addNode(12);
+		tree.addNode(13);
+		tree.addNode(8);
+		tree.addNode(7);
 
-		expect(tree.root.value).to.equal(1);
+		// console.log(JSON.stringify(tree));
+
+		expect(tree.root.value).to.equal(10);
+
+		fs.writeFile(path.join(__dirname + '/tplay.json'), JSON.stringify(tree), (err, data) => {
+			if (err) console.log('error in printing data to json file');
+		});
+	});
+
+	it('should only have two childeren max per node', () => {
+		// expect(tree.root.kids).to.have.lengthOf(2);
 	});
 	it('should have a delete method', () => {});
 	it('should have a search method', () => {});
--- tree/bst/functional.js
+++ tree/bst/functional.js
@@ -17,18 +17,50 @@
 
 const Tree = () => {
 	var tree = {};
-	tree.nodes = {};
 
-	tree.addNode = function(value, kids = []) {
-		console.log('adding node');
+	tree.addNode = function(value) {
 		var node = {};
+		node.kids = [];
 		node.value = value;
-		node.kids = kids;
-		tree.nodes[value] = node;
 
 		if (tree.root === null || tree.root === undefined) {
 			tree.root = node;
+			node.root = true;
+		} else {
+			var recurser = (leaf) => {
+				if (leaf.kids.length === 0 || leaf.kids.length === 1) {
+					leaf.kids.push(node);
+					leaf.kids.sort((a, b) => {
+						return a.value - b.value;
+					});
+				} else {
+					// check to see if value is less than or greater than leaf value
+					var isGreater =
+
+							leaf.value < value ? true :
+							false;
+					//if less run recurser on leaf.kids[0]
+
+					if (isGreater && leaf.kids[1]) {
+						recurser(leaf.kids[1]);
+					} else {
+						recurser(leaf.kids[0]);
+					}
+					// else run recurser on leaf.kids[1]
+
+					// 				1
+					// 			2   3
+					// 	4
+					// 5  6
+
+					// 		1
+					// 2       3
+					// 			 4   5
+				}
+			};
+			recurser(tree.root);
 		}
+
 		//
 	};
 	tree.searchNode = function(value) {
@@ -41,7 +73,6 @@ const Tree = () => {
 		//
 	};
 
-	console.log(JSON.stringify(tree));
 	return tree;
 };
 
